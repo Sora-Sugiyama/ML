d427beb7de8aa983a7362647bf7abdfabd948e3c
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-05-24 23:14:39 +0900

    GradientDescent.h
    Gradient Descent

---
+++ Basic-Header/GradientDescent.h
@@ -0,0 +1,17 @@
+#ifndef LRC_H_INCLUDED
+#define LRC_H_INCLUDED
+
+#include "matrix.h"
+
+vecd GradientDescent(mat A,mat b,mat x,int n){
+    b=Trans(b);x=Trans(x);
+    while(n--){
+        mat r=Subtract(b,Product(A,x));
+        mat rT=Trans(r);
+        double gamma=Product(r,rT)[0][0]/Product(rT,Product(A,r))[0][0];
+        x=Sum(x,ScharProduct(gamma,r));
+    }
+    return Trans(x)[0];
+}
+
+#endif // LRC_H_INCLUDED
