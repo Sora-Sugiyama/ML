a2f9d6eaeb76d02b6eb6de353127dcd02100692b
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-05-25 21:45:07 +0900

    Update GradientDescent.h

--- Basic-Header/Linear-Classfier/GradientDescent.h
+++ Basic-Header/Linear-Classfier/GradientDescent.h
@@ -6,15 +6,14 @@
 
 #include "matrix.h"
 
-vecd GradientDescent(mat A,mat b,mat x,int n){
-    b=Trans(b);x=Trans(x);
+mat GradientDescent(mat A,mat b,mat x,int n){
     while(n--){
         mat r=Subtract(b,Product(A,x));
         mat rT=Trans(r);
         double gamma=Product(r,rT)[0][0]/Product(rT,Product(A,r))[0][0];
         x=Sum(x,ScharProduct(gamma,r));
     }
-    return Trans(x)[0];
+    return x;
 }
 
 #endif // LRC_H_INCLUDED
